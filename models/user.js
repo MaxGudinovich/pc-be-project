45a47cfeaad24b7cf3ad88a9dac2fdb12b51a7bd
Maksym Hudinovych <maksym.hudinovych@powercoders.org>
2024-05-30 12:38:33 +0200

    fix 22

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    photo: { type: Buffer },
+    /* photo: { type: Buffer }, */
   },
   { versionKey: false }
 );
