f97bac5f6088c2d2ad9ce9e4fd6eabc046ccc180
Maksym Hudinovych <maksym.hudinovych@powercoders.org>
2024-05-30 11:55:10 +0200

    fix get with photos

--- api/index.js
+++ api/index.js
@@ -66,11 +66,27 @@ app.post('/users', async (req, res) => {
   }
 });
 
-app.get('/users', async (req, res) => {
+app.get('/users/:id', async (req, res) => {
   try {
-    console.log('Received GET /users request');
-    const users = await User.find();
-    res.status(200).send(users);
+    const userId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).send('Invalid user ID');
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    // Преобразование изображения в base64 перед отправкой
+    const userWithBase64Photo = {
+      ...user.toJSON(),
+      photo: user.photo.toString('base64'),
+    };
+
+    res.status(200).send(userWithBase64Photo);
   } catch (error) {
     res.status(400).send(error.message);
   }
